b5e1b0ed04f3ab1a0d6751393808a313c4ffeaaf
junyeon1997 <junyeon2012@gmail.com>
2020-12-29 22:22:06 +0900

    [FEAT][Docs] Add validation by Flask-wtf
    Complement existing sign up function with Flask-wtf

--- app.py
+++ app.py
@@ -4,30 +4,35 @@ from flask import request
 from flask import redirect
 from flask import render_template
 from model import db
+from flask_wtf.csrf import CSRFProtect
+from forms import RegisterForm
 
 from model import OFFuser
 
 app = Flask(__name__)
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    if request.method == 'POST':    
-        userid = request.form.get('userid')
-        username = request.form.get('username')
-        password = request.form.get('password')
-        re_password = request.form.get('re-password')
+    form = RegisterForm()
+    if form.validate_on_submit(): # 작업요청방식, 유효성검사 한번에 확인
+    # if request.method == 'POST':    
+    #     userid = request.form.get('userid')
+    #     username = request.form.get('username')
+    #     password = request.form.get('password')
+    #     re_password = request.form.get('re-password')
 
-        if (userid and username and password and re_password) and password ==re_password:
-            offuser = OFFuser()
-            offuser.userid = userid
-            offuser.username = username
-            offuser.password = password
-      
-            db.session.add(offuser)
-            db.session.commit()    
+        # if (userid and username and password and re_password) and password ==re_password:->forms-EqualTo
+        offuser = OFFuser()
+        offuser.userid = form.data.get('userid')
+        offuser.username = form.data.get('username')
+        offuser.password = form.data.get('password')
+    
+        db.session.add(offuser)
+        db.session.commit()
+        print('Success!') 
+           
+        return redirect('/')
+    return render_template('register.html', form=form)  
 
-            return redirect('/')
-    return render_template('register.html')  
-      
 @app.route('/')
 def hello():
     return render_template('hello.html')
@@ -39,6 +44,10 @@ if __name__ == "__main__":
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
     app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = 'qwerasdfzxcv'
+
+    csrf = CSRFProtect()
+    csrf.init_app(app)
 
     db.init_app(app)
     db.app = app
