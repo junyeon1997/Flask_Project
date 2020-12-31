aa68b8c77981adc702ec932b7a5632f0a660fdf8
junyeon1997 <junyeon2012@gmail.com>
2020-12-31 15:37:36 +0900

    [Feat] Add login&logout function

--- forms.py
+++ forms.py
@@ -1,3 +1,4 @@
+from model import OFFuser
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms import PasswordField
@@ -9,5 +10,19 @@ class RegisterForm(FlaskForm):
     password = PasswordField('password', validators=[DataRequired(), EqualTo('repassword')])
     repassword = PasswordField('repassword', validators=[DataRequired()])
 
+class LoginForm(FlaskForm):
 
-    
+    class UserPassword(object):
+        def __init__(self, message=None):
+            self.message = message
+
+        def __call__(self, form, field):
+            userid = form['userid'].data
+            password = field.data
+
+            offuser = OFFuser.query.filter_by(userid=userid).first()
+            if offuser.password != password:
+                raise ValueError('Wrong password')
+
+    userid = StringField('userid', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired(), UserPassword()])
